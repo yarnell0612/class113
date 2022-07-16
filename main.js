4833a135327e14a11cdb7fe4fa63d290efd56e97
yarnell0612 <89716653+yarnell0612@users.noreply.github.com>
2022-07-16 21:31:17 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(600, 440);
+    canvas.position(110, 250);
+    video = createCapture(VIDEO);
+    video.hide();
+
+    tint_color = "";
+}
+
+function draw() {
+    image(video, 0, 0, 640, 480);
+    tint(tint_color);
+}
+
+function take_snapshot() {
+    save('pictue.png');
+}
+
+function filter_tint() {
+    tint_color = document.getElementById("color_name").value;
+}
